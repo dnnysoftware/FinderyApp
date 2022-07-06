fcbbc0fe0329c9906230e8adbbf20bb00856eb9c
dnnysoftware <softwarebydanielmironiuk@gmail.com>
2022-07-05 20:13:07 -0400

    yeet

--- dist/finderyapp-f68488b5a938c6de.js
+++ dist/finderyapp-f68488b5a938c6de.js
@@ -513,7 +513,7 @@ function initSync(bytes) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('finderyapp-6bd06e941ec052_bg.wasm', import.meta.url);
+        input = new URL('finderyapp-f68488b5a938c6de_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
