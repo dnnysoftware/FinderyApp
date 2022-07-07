b362a60a265b14b25a35ca875c369c2ab4bcd7a3
dwm9636 <dwm9636@g.rit.edu>
2022-07-07 03:34:33 -0400

    Added search for devices (Couldn't get return error to go away)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,72 @@
+use std::time::SystemTime;
+use futures::StreamExt;
 use yew::prelude::*;
 use web_sys::HtmlInputElement;
 use wasm_bindgen::prelude::*;
+use mongodb::{Client, options::ClientOptions};
+use mongodb::bson::{doc, Document, from_document};
+use mongodb::options::*;
+use mongodb::cursor::Cursor;
 
+mod device;
+
+#[wasm_bindgen(module = "/js/devicemap.js")]
+extern "C"{
+    fn getDeviceFromSearch();
+}
 
 #[wasm_bindgen(module = "/js/devicemap.js")]
 extern "C" {
     fn clearMapSearch();
 }
+
+fn build_device(name: &String) -> device::Device{
+
+    device::Device {
+
+        device_name: ["Looking for ", &name, "... "].concat(),
+        description: String::from("Searching..."),
+        coordinates: (0f32,0f32),
+        battery_life: (0f32),
+        time: SystemTime::now(), //until actual device data after
+        device_ID: 1, //Hard coded for now
+    }
+
+}
+
+// To add data to database from user input: see https://www.youtube.com/watch?v=fm6oinNergw at 20 minutes
+
+async fn search(name: String)-> Result<device::Device, mongodb::error::Error>{
+
+    //before we get data
+
+    let mut dev = build_device(&name);
+
+    //after we get data
+
+    let client_uri = String::from("mongodb+srv://FinderyApp:FinderyApp@cluster0.f6fjujq.mongodb.net");
+
+    //Connecting to database
+    let client_options = ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare()).await?;
+    let client = Client::with_options(client_options)?;
+    let modules = client.database("FinderyApp").collection("Modules");
+
+    //Finding the correct element
+    let query = doc!{"TrackerID": dev.device_ID};
+    let find_options = FindOptions::builder().sort(doc!{"TrackerID": dev.device_ID}).build();
+    let mut cursor = modules.find(query, find_options).await?;
+
+    //Getting the values
+    let mut counter = 0;
+    while let Some(result) = cursor.next().await {
+        let dev = from_document(result?);
+    }
+
+    //returning initial device
+    dev
+
+}
+
 
 #[function_component(App)]
 fn app() -> Html {
